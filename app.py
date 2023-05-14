f7cf1a5c8779cc3bd958dc0df2af27e9cac9ebe4
AKACHI-4 <Adarshrawat8304@gmail.com>
2023-05-14 07:41:59 +0530

    (json->yaml)

--- app.py
+++ app.py
@@ -1,14 +1,27 @@
-from flask import Flask
+from flask import Flask, request
 from flask_cors import CORS
+import yaml
 
 app = Flask(__name__)
-CORS(app)
+app.config['YAML_AS_TEXT'] = True
 
 @app.route('/members', methods=['GET'])
 def members():    
     return {"members": ["member1", "member2", "member3"]}
 
+@app.route('/class-data', methods=['POST', 'GET'])
+def classdata():
+    data = yaml.safe_load(request.data)
 
-if __name__ == '__main__':
+    return { 'data' : data }
+
+@app.route('/student-data', methods=['POST', 'GET'])    
+def studentdata():
+    data = yaml.safe_load(request.data)
+
+    return { 'data' : data }
+
+if __name__ == "__main__":
     app.run(debug=True)
+
     
