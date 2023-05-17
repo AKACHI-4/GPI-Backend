111a988e7c5584011ae32b2a8ee0c7c866e9a48b
AKACHI-4 <Adarshrawat8304@gmail.com>
2023-05-17 22:28:54 +0530

    Database connection made

--- app.py
+++ app.py
@@ -1,25 +1,33 @@
 from flask import Flask, request
 from flask_cors import CORS
 import yaml
+from flask_pymongo import PyMongo 
 
 app = Flask(__name__)
 app.config['YAML_AS_TEXT'] = True
-
-@app.route('/members', methods=['GET'])
-def members():    
-    return {"members": ["member1", "member2", "member3"]}
+app.config["MONGO_URI"] = "mongodb://localhost:27017/admin"
+mongo = PyMongo(app)
+db = mongo.db
 
 @app.route('/class-data', methods=['POST', 'GET'])
 def classdata():
     data = yaml.safe_load(request.data)
-
-    return { 'data' : data }
+    classdata = db.get_collection('class_data')
+    classdata.insert_one(data)
+    return { 'Output' : ' Data inserted successfully ' }
 
 @app.route('/student-data', methods=['POST', 'GET'])    
 def studentdata():
     data = yaml.safe_load(request.data)
+    studentdata = db.get_collection('student_data')
+    studentdata.insert_one(data)
+    return { 'Output' : ' Data inserted successfully ' }
 
-    return { 'data' : data }
+@app.route('/class-room', methods=['POST', 'GET'])
+def classRoom():
+    
+
+    return {}
 
 if __name__ == "__main__":
     app.run(debug=True)
