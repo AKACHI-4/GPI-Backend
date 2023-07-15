fe4ec4ed9a8c1661607791f5352631911da9f8d0
AKACHI-4 <Adarshrawat8304@gmail.com>
2023-07-15 22:08:57 +0530

    fix: Student form route fixed

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, Response, jsonify
+from flask import Flask, request, Response, jsonify, redirect
 from flask_cors import CORS
 import yaml
 from flask_pymongo import PyMongo 
@@ -11,11 +11,11 @@ app.config['YAML_AS_TEXT'] = True
 app.config["MONGO_URI"] = "mongodb://localhost:27017/admin"
 mongo = PyMongo(app)
 db = mongo.db
-
+    
+unique_id = str(uuid.uuid4())
 @app.route('/generate-link/<string:admin_id>', methods=['GET'])
 def generate_link(admin_id):
-    unique = str(uuid.uuid4())
-    unique_link = f"localhost:3000/student-form/{admin_id}/{unique}"
+    unique_link = f"localhost:3000/student-form/{admin_id}/{unique_id}"
     return { 'link' : unique_link }
 
 @app.route('/class-data', methods=['POST', 'GET'])
@@ -30,34 +30,37 @@ def classdata():
     else:
         return { 'Error' : 'No data provided' }, 400
 
-@app.route('/student-data/<string:admin_id>/<string:uuid>', methods=['POST', 'GET'])    
-def studentdata(admin_id, uuid):
-    if request.data:
-        data = yaml.safe_load(request.data)
+@app.route('/student-data/<string:admin_id>/<string:provided_id>', methods=['POST'])    
+def studentdata(admin_id, provided_id):
+    if provided_id == unique_id:
+        if request.data:
+            data = yaml.safe_load(request.data)
 
-        classdata = db.get_collection('class_data')
-        class_info = classdata.find_one({})
+            classdata = db.get_collection('class_data')
+            class_info = classdata.find_one({})
 
-        radius = float(class_info['radius'])
+            radius = float(class_info['radius'])
 
-        admin_coord = (class_info['latitude'], class_info['longitude'])
-        user_coord = (data['latitude'], data['longitude'])        
+            admin_coord = (class_info['latitude'], class_info['longitude'])
+            user_coord = (data['latitude'], data['longitude'])        
 
-        distance = float(haversine(admin_coord, user_coord, unit=Unit.METERS))
+            distance = float(haversine(admin_coord, user_coord, unit=Unit.METERS))
 
-        data['present'] = distance < radius
+            data['present'] = distance < radius
 
-        studentdata = db.get_collection('student_data')
-        studentdata.insert_one(data)
-        return { 'Output' : 'Data inserted successfully' }
+            studentdata = db.get_collection('student_data')
+            studentdata.insert_one(data)
+            return { 'Output' : 'Data inserted successfully' }
+        else:
+            return { 'Error' : 'No data provided' }, 400
     else:
-        return { 'Error' : 'No data provided' }, 400
+        return { 'Error' : 'Invalid Unique ID' }, 400
 
-@app.route('/GetClassData', methods=['GET'])
+@app.route('/getClassData', methods=['GET'])
 def GetClassData():
     class_data = db.get_collection('class_data')
 
-    result = class_data.find_one()
+    result = class_data.find()
     data = list(result)
 
     for item in data :
@@ -65,7 +68,7 @@ def GetClassData():
 
     return jsonify(data)
 
-@app.route('/GetStudentData', methods=['GET'])
+@app.route('/getStudentData', methods=['GET'])
 def GetStudentData():
     student_data = db.get_collection('student_data')
 
@@ -79,4 +82,3 @@ def GetStudentData():
 
 if __name__ == "__main__":
     app.run(debug=True)    
-
