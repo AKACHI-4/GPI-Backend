8163a15178850966f7092fe99c712d954156ce75
AKACHI-4 <Adarshrawat8304@gmail.com>
2023-07-17 20:26:31 +0530

    deploy: Made necessary changes for the deployment

--- app.py
+++ app.py
@@ -1,21 +1,24 @@
 from flask import Flask, request, Response, jsonify, redirect
 from flask_cors import CORS
 import yaml
-from flask_pymongo import PyMongo 
-from bson import json_util
 from haversine import haversine, Unit
+from google.cloud import firestore 
+import google.cloud.exceptions
 import uuid
+import os
 
 app = Flask(__name__)
 app.config['YAML_AS_TEXT'] = True
-app.config["MONGO_URI"] = "mongodb://localhost:27017/admin"
-mongo = PyMongo(app)
-db = mongo.db
+firestore_client = firestore.Client()
     
+@app.route('/')
+def home():
+    return "Secure, You are in GPI Backend !!"
+
 unique_id = str(uuid.uuid4())
 @app.route('/generate-link/<string:admin_id>', methods=['GET'])
 def generate_link(admin_id):
-    unique_link = f"localhost:3000/student-form/{admin_id}/{unique_id}"
+    unique_link = f"http://gpi.software/student-form/{admin_id}/{unique_id}"
     return { 'link' : unique_link }
 
 @app.route('/class-data', methods=['POST', 'GET'])
@@ -23,8 +26,8 @@ def classdata():
     if request.data:
         data = yaml.safe_load(request.data)
 
-        classdata = db.get_collection('class_data')
-        classdata.insert_one(data)
+        classData = firestore_client.collection('class_data').document()
+        classData.set(data)
 
         return { 'Output' : 'Data inserted successfully' }
     else:
@@ -32,25 +35,32 @@ def classdata():
 
 @app.route('/student-data/<string:admin_id>/<string:provided_id>', methods=['POST'])    
 def studentdata(admin_id, provided_id):
+    print(provided_id, unique_id)
     if provided_id == unique_id:
         if request.data:
             data = yaml.safe_load(request.data)
 
-            classdata = db.get_collection('class_data')
-            class_info = classdata.find_one({})
+            class_data = firestore_client.collection('class_data')
+            class_info = class_data.limit(1).get()
 
-            radius = float(class_info['radius'])
+            if len(class_info) > 0:
+                # Access the first document in the list
+                first_doc = class_info[0].to_dict()
 
-            admin_coord = (class_info['latitude'], class_info['longitude'])
-            user_coord = (data['latitude'], data['longitude'])        
+                radius = float(first_doc['radius'])
 
-            distance = float(haversine(admin_coord, user_coord, unit=Unit.METERS))
+                admin_coord = (first_doc['latitude'], first_doc['longitude'])
+                user_coord = (data['latitude'], data['longitude'])
 
-            data['present'] = distance < radius
+                distance = float(haversine(admin_coord, user_coord, unit=Unit.METERS))
 
-            studentdata = db.get_collection('student_data')
-            studentdata.insert_one(data)
-            return { 'Output' : 'Data inserted successfully' }
+                data['present'] = distance < radius
+
+                studentData = firestore_client.collection('student_data').document()
+                studentData.set(data)
+                return {'Output': 'Data inserted successfully'}
+            else:
+                return {'Error': 'No class data found'}, 400
         else:
             return { 'Error' : 'No data provided' }, 400
     else:
@@ -58,27 +68,32 @@ def studentdata(admin_id, provided_id):
 
 @app.route('/getClassData', methods=['GET'])
 def GetClassData():
-    class_data = db.get_collection('class_data')
+    class_data = firestore_client.collection('class_data')
+    results = class_data.limit(1).get()
 
-    result = class_data.find()
-    data = list(result)
+    if len(results) > 0:
+        data = []
+        for doc in results:
+            data.append(doc.to_dict())
 
-    for item in data :
-        item['_id'] = str(item['_id'])
-
-    return jsonify(data)
+        return jsonify(data)
+    else:
+        return jsonify({})
 
 @app.route('/getStudentData', methods=['GET'])
 def GetStudentData():
-    student_data = db.get_collection('student_data')
+    student_data = firestore_client.collection('student_data')
+    results = student_data.get()
 
-    result = student_data.find()
-    data = list(result)
+    if len(results) > 0:
+        data = []
+        for doc in results:
+            data.append(doc.to_dict())
 
-    for item in data :
-        item['_id'] = str(item['_id'])
-
-    return jsonify(data)
+        return jsonify(data)
+    else:
+        return jsonify({})
 
 if __name__ == "__main__":
-    app.run(debug=True)    
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+ 
